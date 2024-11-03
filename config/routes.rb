530e363eeefd7a4ddec89a85d73f6d077d8b6936
emkang6 <hkang6@uchicago.edu>
2024-11-03 00:46:29 +0000

    Adds directors#index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index"})
+
+  # get("/directors", { :controller => "", :action => ""})
+
+  # get("/directors", { :controller => "", :action => ""})
+
+  # get("/directors", { :controller => "", :action => ""})
 end
